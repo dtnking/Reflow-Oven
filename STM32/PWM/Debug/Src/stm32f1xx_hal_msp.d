2646f9cac3c22e1499a7cb5783b8a860bbac3b78
Danny Aw <kokwah07@hotmail.com>
2018-06-06 01:23:11 +0800

    created fake signal using STM32

---
+++ STM32/PWM/Debug/Src/stm32f1xx_hal_msp.d
@@ -0,0 +1,70 @@
+Src/stm32f1xx_hal_msp.o: ../Src/stm32f1xx_hal_msp.c \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ D:/Project/c/STM32/PWM/PWM/Inc/stm32f1xx_hal_conf.h \
+ D:/Project/c/STM32/PWM/PWM/Inc/main.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/core_cm3.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+D:/Project/c/STM32/PWM/PWM/Inc/stm32f1xx_hal_conf.h:
+
+D:/Project/c/STM32/PWM/PWM/Inc/main.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/core_cm3.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+D:/Project/c/STM32/PWM/PWM/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
